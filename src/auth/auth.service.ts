8d7f4bae59ff58eb949633c0ca57c0d62332432e
Ivan <ivan18drozdov@gmail.com>
2025-03-13 23:14:17 +0300

    working auth w/ cookies

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,9 +3,13 @@ import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
 import { AuthDto } from './dto/auth.dto';
 import { verify } from 'argon2';
+import { Response } from 'express';
 
 @Injectable()
 export class AuthService {
+    EXPIRE_DAY_REFRESH_TOKEN = 1
+    REFRESH_TOKEN_NAME = 'refreshToken'
+
     constructor(
         private jwt: JwtService,
         private userService: UserService
@@ -28,9 +32,23 @@ export class AuthService {
         if (oldUser) throw new BadRequestException('User already exists')
 
         const { password, ...user } = await this.userService.create(dto)
-        
+
         const tokens = this.issueTokens(user.id)
 
+        return {
+            user,
+            ...tokens,
+        }
+    }
+
+    async getNewTokens(refreshToken: string){
+        const result = await this.jwt.verifyAsync(refreshToken)
+        if (!result) throw new UnauthorizedException('Invalid refresh token')
+
+        const { password, ...user } = await this.userService.getById(result.id)
+
+        const tokens = this.issueTokens(user.id)
+        
         return {
             user,
             ...tokens,
@@ -60,5 +78,27 @@ export class AuthService {
         if (!isValid) throw new UnauthorizedException('Invalid password')
 
         return user
+    }
+
+    addRefreshTokenToResponse(res: Response, refreshToken: string) {
+        const expiresIn = new Date()
+        expiresIn.setDate(expiresIn.getDate() + this.EXPIRE_DAY_REFRESH_TOKEN)
+        res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, {
+            httpOnly: true,
+            domain: 'localhost',
+            expires: expiresIn,
+            secure: true,
+            sameSite: 'none' // заменить на lax для прода
+        })
+    }
+
+    removeRefreshTokenFromResponse(res: Response) {
+        res.cookie(this.REFRESH_TOKEN_NAME, '', {
+            httpOnly: true,
+            domain: 'localhost',
+            expires: new Date(0),
+            secure: true,
+            sameSite: 'none' // заменить на lax для прода
+        })
     }
 }
