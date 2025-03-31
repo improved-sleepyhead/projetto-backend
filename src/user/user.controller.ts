ca36969575ce3896a62b5b8c996bbaca8e87e0b7
Ivan <ivan18drozdov@gmail.com>
2025-03-31 22:10:21 +0300

    updated user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,27 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UserService } from './user.service';
 
-
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get(':id/profile')
+  async getProfile(@Param('id') id: string) {
+    return this.userService.getProfile(id);
+  }
+
+  @Post()
+  async create(@Body() dto: any) {
+    return this.userService.create(dto);
+  }
+
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() dto: any) {
+    return this.userService.update(id, dto);
+  }
+
+  // @Delete(':id')
+  // async delete(@Param('id') id: string) {
+  //   return this.userService.delete(id);
+  // }
 }
