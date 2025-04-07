67134f69b99b2647fdca3bf6aa8d5d886ff95dab
Ivan <ivan18drozdov@gmail.com>
2025-04-07 22:42:25 +0300

    other modules init

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { ProjectModule } from './project/project.module';
+import { CommentModule } from './comment/comment.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, UserModule]
+  imports: [ConfigModule.forRoot(), AuthModule, UserModule, ProjectModule, CommentModule, TaskModule]
 })
 export class AppModule {}
