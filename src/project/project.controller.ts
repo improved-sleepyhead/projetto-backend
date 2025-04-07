e8d83abebcf1b633dc42faeae7b0b77de5d8229c
Ivan <ivan18drozdov@gmail.com>
2025-04-08 00:55:14 +0300

    project controller

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -1,34 +1,67 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { ProjectService } from './project.service';
-import { CreateProjectDto } from './dto/project.dto';
-import { UpdateProjectDto } from './dto/update-project.dto';
+import {Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, UsePipes, ValidationPipe, Query} from '@nestjs/common';
+import { Auth } from '../auth/decorators/auth.decorator';
 
-@Controller('project')
+import { ProjectService } from './project.service';
+
+import { CreateProjectDto, ProjectDto, UpdateProjectDto } from './dto/project.dto';
+import { CurrentUser } from 'src/auth/decorators/user.decorator';
+
+@Controller('projects')
 export class ProjectController {
   constructor(private readonly projectService: ProjectService) {}
 
   @Post()
-  create(@Body() createProjectDto: CreateProjectDto) {
-    return this.projectService.create(createProjectDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.projectService.findAll();
+  @Auth()
+  @UsePipes(new ValidationPipe())
+  async create(
+    @Body() dto: CreateProjectDto,
+    @CurrentUser('id') userId: string,
+  ): Promise<ProjectDto> {
+    return this.projectService.create(dto, userId);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.projectService.findOne(+id);
+  @Auth()
+  async getById(
+    @Param('id') id: string,
+    @Query('includeTimestamps') includeTimestamps: boolean,
+  ): Promise<ProjectDto> {
+    return this.projectService.getById(id, includeTimestamps);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProjectDto: UpdateProjectDto) {
-    return this.projectService.update(+id, updateProjectDto);
+  @Auth()
+  @UsePipes(new ValidationPipe())
+  async update(
+    @Param('id') id: string,
+    @Body() dto: UpdateProjectDto,
+  ): Promise<ProjectDto> {
+    return this.projectService.update(id, dto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.projectService.remove(+id);
+  @HttpCode(204)
+  @Auth()
+  async delete(@Param('id') id: string): Promise<void> {
+    return this.projectService.delete(id);
+  }
+
+  @Post(':projectId/members/:userId')
+  @Auth()
+  async addMember(
+    @Param('projectId') projectId: string,
+    @Param('userId') userId: string,
+  ): Promise<ProjectDto> {
+    return this.projectService.addMember(projectId, userId);
+  }
+
+  @Delete(':projectId/members/:userId')
+  @HttpCode(204)
+  @Auth()
+  async removeMember(
+    @Param('projectId') projectId: string,
+    @Param('userId') userId: string,
+  ): Promise<ProjectDto> {
+    return this.projectService.removeMember(projectId, userId);
   }
 }
