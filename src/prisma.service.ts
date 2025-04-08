2d789f7befff32bdc7c9db18bac51497451c2f7e
Ivan <ivan18drozdov@gmail.com>
2025-04-08 22:40:34 +0300

    hotfix trash json response fields by middleware

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -5,5 +5,30 @@ import { PrismaClient } from "@prisma/client";
 export class PrismaService extends PrismaClient implements OnModuleInit {
     async onModuleInit() {
         await this.$connect()
+        this.$use(async (params, next) => {
+            const result = await next(params);
+      
+            const cleanSensitiveFields = (data: any): any => {
+              if (!data || typeof data !== 'object') return data;
+      
+              if (Array.isArray(data)) {
+                return data.map(cleanSensitiveFields);
+              }
+      
+              delete data.password;
+              delete data.createdAt;
+              delete data.updatedAt;
+      
+              for (const key in data) {
+                if (data.hasOwnProperty(key)) {
+                  data[key] = cleanSensitiveFields(data[key]);
+                }
+              }
+      
+              return data;
+            };
+      
+            return cleanSensitiveFields(result);
+        });
     }
 }
