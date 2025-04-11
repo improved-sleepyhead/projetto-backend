24690ff2043db64ba03c0595ecd7ebd699a341e4
Ivan <ivan18drozdov@gmail.com>
2025-04-11 17:00:53 +0300

    addode role controller, role module is finished

--- src/role/role.controller.ts
+++ src/role/role.controller.ts
@@ -1,34 +1,35 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Patch,
+  Param,
+  Body,
+  HttpCode,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Auth } from '../auth/decorators/auth.decorator';
+import { Roles } from '../project/decorators/roles.decorator';
 import { RoleService } from './role.service';
-import { CreateRoleDto } from './dto/role.dto';
-import { UpdateRoleDto } from './dto/update-role.dto';
+import { UpdateRoleDto } from './dto/role.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
+import { ProjectRolesGuard } from '../project/guards/project-routes.guard';
 
-@Controller('role')
+@Controller('projects/:projectId/roles')
+@UseGuards(JwtAuthGuard, ProjectRolesGuard)
 export class RoleController {
   constructor(private readonly roleService: RoleService) {}
 
-  @Post()
-  create(@Body() createRoleDto: CreateRoleDto) {
-    return this.roleService.create(createRoleDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.roleService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.roleService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto) {
-    return this.roleService.update(+id, updateRoleDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.roleService.remove(+id);
+  @Patch(':userId')
+  @HttpCode(204)
+  @Auth()
+  @Roles('PROJECT_ADMIN', 'MANAGER')
+  @UsePipes(new ValidationPipe({ transform: true }))
+  async updateRole(
+    @Param('projectId') projectId: string,
+    @Param('userId') userId: string,
+    @Body() dto: UpdateRoleDto,
+  ): Promise<void> {
+    return this.roleService.updateRole(projectId, userId, dto);
   }
 }
--- src/role/role.module.ts
+++ src/role/role.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { RoleService } from './role.service';
 import { RoleController } from './role.controller';
+import { PrismaService } from 'src/prisma.service';
+import { ProjectService } from 'src/project/project.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [RoleController],
-  providers: [RoleService],
+  providers: [RoleService, PrismaService, ProjectService],
 })
 export class RoleModule {}
