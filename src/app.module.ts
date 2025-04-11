554daa101914dea520152d8f1746f401a7706127
Ivan <ivan18drozdov@gmail.com>
2025-04-11 14:01:51 +0300

    new role module init

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,9 @@ import { ConfigModule } from '@nestjs/config';
 import { ProjectModule } from './project/project.module';
 import { CommentModule } from './comment/comment.module';
 import { TaskModule } from './task/task.module';
+import { RoleModule } from './role/role.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, UserModule, ProjectModule, CommentModule, TaskModule]
+  imports: [ConfigModule.forRoot(), AuthModule, UserModule, ProjectModule, CommentModule, TaskModule, RoleModule]
 })
 export class AppModule {}
